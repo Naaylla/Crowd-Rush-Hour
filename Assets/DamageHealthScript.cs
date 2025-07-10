5fba7a524234819603d283045a0a4c59f6b5a1dc
YacIneTheBox <yacaitali@gmail.com>
2025-07-10 19:23:47 +0100

    adding hearts to the music scene

--- Assets/DamageHealthScript.cs
+++ Assets/DamageHealthScript.cs
@@ -1,20 +1,43 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class DamageHealthScript : MonoBehaviour
 {
-    public Sprite heart;
-    public int pv = 3;
+    public Sprite heart;               // Sprite image for the heart
+    public int pv = 3;                 // Number of lives
+    public GameObject heartContainer;  // UI GameObject to hold the hearts
+    public GameObject heartPrefab;     // Prefab with an Image component
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private List<GameObject> heartIcons = new List<GameObject>();
+
     void Start()
     {
-                
+        DrawHearts();
     }
 
-    // Update is called once per frame
-    void Update()
+    void DrawHearts()
     {
-        
+        // Clean existing hearts
+        foreach (GameObject obj in heartIcons)
+        {
+            Destroy(obj);
+        }
+        heartIcons.Clear();
+
+        // Create new hearts
+        for (int i = 0; i < pv; i++)
+        {
+            GameObject newHeart = Instantiate(heartPrefab, heartContainer.transform);
+            newHeart.GetComponent<Image>().sprite = heart;
+            heartIcons.Add(newHeart);
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        pv -= amount;
+        pv = Mathf.Max(pv, 0); // Prevent negative values
+        DrawHearts();
     }
 }
